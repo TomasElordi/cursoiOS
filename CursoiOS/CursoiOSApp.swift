967c61b9f73f4cc301fa155fee0a71317590986e
TomasElordi <tomaselordi3@gmail.com>
2024-10-16 07:10:55 -0300

    Initial Commit

---
+++ CursoiOS/CursoiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CursoiOSApp.swift
+//  CursoiOS
+//
+//  Created by Tomas Elordi on 16/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct CursoiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
